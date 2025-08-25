6c8fe9e9e472799d03acf3ee2d6e10cc868f733e
Hardik Teotia <130360009+hardikteotia@users.noreply.github.com>
2025-08-25 18:22:42 +0000

    another assignment started

---
+++ Day4Assignments/Ques1.cpp
@@ -0,0 +1,82 @@
+/*1:Create Date class with members day,month ,year.
+Write no argument and parameterised constructor. Create two objects and initialize them using no 
+argument and parameterised constructor
+respectively.Print date using display function.*/
+
+#include <iostream>
+using namespace std;
+
+class Date{
+    private:
+            int dd,mm,yy;
+    public:
+            //constructors
+            Date(){
+                cout<<"\n------In default constructor-----\n";
+                dd=0;
+                mm=0;
+                yy=0;
+            }
+
+            Date(int dd, int mm, int yy){
+                cout<<"\n------In parameterised constructor-----\n";
+                this->dd=dd;
+                this->mm=mm;
+                this->yy=yy;
+            }
+
+            //getters
+            int getDay();
+            int getMonth();
+            int getYear();
+
+            //setters
+            void setDay(int dd);
+            void setMonth(int mm);
+            void setYear(int yy);
+
+            //display function
+            void display(){
+                cout<<"\nDay: "<<dd<<" Month: "<<mm<<" Year: "<<yy<<"\n";
+            }
+
+};
+
+
+int Date:: getDay(){
+    return dd;
+}
+int Date:: getMonth(){
+    return mm;
+}
+int Date:: getYear(){
+    return yy;
+}
+
+void Date:: setDay(int dd){
+    this->dd=dd;
+}
+void Date:: setMonth(int mm){
+    this->mm=mm;
+}
+void Date:: setYear(int yy){
+    this->yy=yy;
+}
+
+
+int main() {
+     Date D1;
+    D1.setDay(10);
+    D1.setMonth(4);
+    D1.setYear(2003);
+
+    int day1 = D1.getDay();
+    int month1 = D1.getMonth();
+    int Year1 = D1.getYear();
+
+    D1.display();
+
+    Date D2(4,10,2003);
+    D2.display();
+    return 0;
+}
